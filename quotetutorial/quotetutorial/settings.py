0413a1de9704dc770b8ccc12da26fceab9b5ffa4
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-07-09 20:27:16 +0530

    Made ROBOTS.TXT False
    Signed-off-by: mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>

--- quotetutorial/quotetutorial/settings.py
+++ quotetutorial/quotetutorial/settings.py
@@ -18,7 +18,7 @@ NEWSPIDER_MODULE = 'quotetutorial.spiders'
 # USER_AGENT = 'quotetutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
